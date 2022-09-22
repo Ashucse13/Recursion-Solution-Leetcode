3aadc8de26a0d6ee72a25e971ddd777776d9b627
Ashucse13 <86155592+Ashucse13@users.noreply.github.com>
2022-09-23 00:07:51 +0530

    Time: 0 ms (100.00%), Space: 38.9 MB (96.39%) - LeetHub

--- 62-unique-paths/62-unique-paths.java
+++ 62-unique-paths/62-unique-paths.java
@@ -1,34 +1,29 @@
 class Solution {
-    static int countWaysUtil(int m, int n, int[][] dp) {
-    for(int i=0; i<m ;i++){
-      for(int j=0; j<n; j++){
-          
-          //base condition
-          if(i==0 && j==0){
-              dp[i][j]=1;
-              continue;
-          }
-          
-          int up=0;
-          int left = 0;
-          
-          if(i>0) 
-            up = dp[i-1][j];
-          if(j>0)
-            left = dp[i][j-1];
-            
-          dp[i][j] = up+left;
-      }
-  }
-  
-  return dp[m-1][n-1];
-
-}
     public int uniquePaths(int m, int n) {
-        int dp[][]=new int[m][n];
-    for (int[] row : dp)
-        Arrays.fill(row, -1);
-    return countWaysUtil(m,n,dp);
+        int prev[]=new int[n];
+    for(int i=0; i<m; i++){
+        int temp[]=new int[n];
+        for(int j=0; j<n; j++){
+            if(i==0 && j==0){
+                temp[j]=1;
+                continue;
+            }
+            
+            int up=0;
+            int left =0;
+            
+            if(i>0)
+                up = prev[j];
+            if(j>0)
+                left = temp[j-1];
+                
+            temp[j] = up + left;
+        }
+        prev = temp;
+    }
+    
+    return prev[n-1];
+    
         
     }
 }
