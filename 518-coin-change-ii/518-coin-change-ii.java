5f052ed3359297a523a6bf3bbb24f8d4122a3ede
Ashucse13 <86155592+Ashucse13@users.noreply.github.com>
2022-10-08 23:34:40 +0530

    Time: 24 ms (21.69%), Space: 50.5 MB (41.33%) - LeetHub

--- 518-coin-change-ii/518-coin-change-ii.java
+++ 518-coin-change-ii/518-coin-change-ii.java
@@ -1,15 +1,31 @@
 class Solution {
     public int change(int amount, int[] coins) {
-        int dp[] = new int[amount+1];
-        for(int i=0;i<=amount;i++){
-            dp[i] = 0;
+       int[] prev=new int[amount+1];
+        int n=coins.length;
+    
+    
+    //Initializing base condition
+    for(int i=0;i<=amount;i++){
+        if(i%coins[0]==0)
+            prev[i]=1;
+        // Else condition is automatically fulfilled,
+        // as prev array is initialized to zero
+    }
+    
+    for(int ind=1; ind<n;ind++){
+        int cur[]=new int[amount+1];
+        for(int target=0;target<=amount;target++){
+            int notTaken = prev[target];
+            
+            int taken = 0;
+            if(coins[ind]<=target)
+                taken = cur[target-coins[ind]];
+                
+            cur[target] = notTaken + taken;
         }
-        dp[0] = 1;
-        for(int i=0;i<coins.length;i++){
-            for(int j=coins[i];j<=amount;j++){
-                dp[j] += dp[j-coins[i]];
-            }
-        }
-        return dp[amount];
+        prev = cur;
+    }
+    
+    return prev[amount];
     }
 }
